ab24339561001d808492974dba8de5e663c09f83
abdel-elsayed <abdel.elsayed18@gmail.com>
2021-11-21 21:02:01 -0500

    Initial Commit

---
+++ WalletApp/WalletAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletAppApp.swift
+//  WalletApp
+//
+//  Created by Abdelrahman Elsayed on 11/21/21.
+//
+
+import SwiftUI
+
+@main
+struct WalletAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
